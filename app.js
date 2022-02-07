01ee43a95116ac599bd7c5c82b23db28a2c6fe84
Ahmet Meliksah Akdeniz <aahmetmeliksah@gmail.com>
2022-02-07 17:55:25 +0300

    partials made | footer, header, navigation files added

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require("express"); // imported express
 const ejs = require("ejs");
 const app = express(); // assigned express func to app constant
 
+// ENGINE SET
 app.set("view engine", "ejs"); // set engine as ejs that looks at view folder
 app.use(express.static("public")); // look at public folder for static parts of the webapp
 
-// When root URL is entered, it sends response of blog object
+// ROUTES
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-// Lisntens to the port at 3000
+// LISTEN PORT
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server started working at port ${PORT}`);
